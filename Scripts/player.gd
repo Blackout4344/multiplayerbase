799984fb986fcb3e37175d9ce6ae8ef4baf7c4c4
Blackout4344 <mohammad.khan6823@gmail.com>
2024-10-17 16:44:35 -0700

    sync pos

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -34,7 +34,6 @@ func _enter_tree():
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	camera.current = is_multiplayer_authority()
-	mesh.visible = is_multiplayer_authority()
 	
 	
 	
